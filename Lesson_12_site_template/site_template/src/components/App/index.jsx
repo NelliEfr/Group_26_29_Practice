9abdc6b29a7f235386f57a3861066741c76ce30c
Nelli Efremyan <n.efremyan@gmail.com>
2022-09-02 13:24:06 +0400

    navbar added

---
+++ Lesson_12_site_template/site_template/src/components/App/index.jsx
@@ -0,0 +1,12 @@
+import '../../style.css'
+import Navbar from "../Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ Lesson_12_site_template/site_template/src/components/Navbar/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import logo from './media/buh_one_logo.png'
+import s from './style.module.css'
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div className={['wrapper', s.wrapper].join(' ')}>
+        <a href='#'>
+          <img src={logo} alt='logo' />
+        </a>
+        <ul className={s.menu}>
+          <li>Главная</li>
+          <li>Услуги</li>
+          <li>Кейсы</li>
+          <li>О компании</li>
+          <li>Контакты</li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
