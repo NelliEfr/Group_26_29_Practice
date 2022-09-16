c5e6957eed4b40f269a10421e3f9202f89975bda
Nelli Efremyan <n.efremyan@gmail.com>
2022-09-16 13:29:59 +0400

    about_us block finished

--- Lesson_12_site_template/site_template/src/components/AboutUs/index.jsx
+++ Lesson_12_site_template/site_template/src/components/AboutUs/index.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import AboutUsCard from '../AboutUsCard'
+import s from './style.module.css'
 import {data} from './data'
 
 export default function AboutUs() {
   return (
-    <section>
+    <section className={['wrapper', s.about_us].join(' ')}>
       <div>
         <p className='subheader'>О нас</p>
-        <h2>Компания ИванПром</h2>
-        <p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн.</p>
+        <h2>Компания <span className={s.accent}>ИванПром</span></h2>
+        <p className={s.infoText}>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн.</p>
       </div>
 
-      <div>
+      <div className={s.achievements}>
         {
           data.map(el => <AboutUsCard key={el.id} {...el}/>)
         }
--- Lesson_12_site_template/site_template/src/components/AboutUsCard/index.jsx
+++ Lesson_12_site_template/site_template/src/components/AboutUsCard/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import s from './style.module.css'
 
 export default function AboutUsCard({value, title, link_title, link_url}) {
   return (
-    <div>
-      <p>{value}</p>
-      <p>{title}</p>
+    <div className={s.card_item}>
+      <p className={s.value}>{value}</p>
+      <p className={s.title}>{title}</p>
       <a href={link_url}>{link_title}</a>
     </div>
   )
